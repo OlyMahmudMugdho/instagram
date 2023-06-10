a999079cc8479d5e37a2c464b273f97d11343aa3
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-10 20:52:49 +0600

    Add files via upload

--- models/Post.js
+++ models/Post.js
@@ -5,22 +5,32 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema(
     {
         content: {
-            type : String,
-            required : true
-        },
-        author : {
             type: String,
-            required : true
+            required: true
         },
-        postId : {
+        imageUrl: {
             type : String,
             required : true
         },
-        date : {
-            type : Date,
-            default : Date.now()
-        }
+        author: {
+            type: String,
+            required: true
+        },
+        userID : {
+            type : String,
+            required : true
+        },
+        postId: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now()
+        },
+        likes: Number,
+        comments: Number,
     }
 );
 
-module.exports = mongoose.model('Posts',postSchema);
+module.exports = mongoose.model('Posts', postSchema);
