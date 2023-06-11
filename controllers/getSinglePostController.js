d04de42ec7c901bff63df0be8942a574d46422ee
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-11 12:50:24 +0600

    Add files via upload

---
+++ controllers/getSinglePostController.js
@@ -0,0 +1,25 @@
+const Post = require('../models/Post');
+
+const getSinglePost = async (req, res) => {
+    const userID = req.params.postUserID;
+    const postId = req.params.postId;
+
+    console.log({ userID: userID, postId: postId });
+
+    const foundPost = await Post.findOne({ userID: userID, postId: postId });
+
+    if (!foundPost) {
+        return res.status(404).json({
+            error: "true",
+            message: "post not found"
+        });
+    }
+
+    return res.status(200).json({
+        data: foundPost
+    })
+}
+
+module.exports = {
+    getSinglePost
+};
