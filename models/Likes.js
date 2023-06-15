da612cb52fdd1076d2381fe5944e9ea3b7de41e5
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-15 21:05:47 +0600

    Add files via upload

---
+++ models/Likes.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const likesSchema = new Schema({
+    userID: {
+        type: String,
+        required: true
+    },
+    postId: {
+        type: String,
+        required: true
+    },
+    likedAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+module.exports = mongoose.model('Likes', likesSchema);
