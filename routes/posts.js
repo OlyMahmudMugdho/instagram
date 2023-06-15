3497eb30db47ee792c2088ff656fd989e980b85a
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-15 21:06:26 +0600

    Add files via upload

--- routes/posts.js
+++ routes/posts.js
@@ -9,6 +9,8 @@ const verifyAccessToken = require('../middlewares/verifyAccessToken');
 const fileUpload = require('express-fileupload');
 const editPostController = require('../controllers/editPostController');
 const getSinglePostController = require('../controllers/getSinglePostController');
+const deletePostController = require('../controllers/deletePostController');
+const change = require('../middlewares/changeParam').change;
 
 require('dotenv').config();
 
@@ -16,8 +18,12 @@ router.route('/posts')
     .get(checkRoute.checkUrl, checkJWT.check, verifyAccessToken.verifyAccess, allPostsController.showAllPosts)
     .post(verifyAccessToken.verifyAccess, fileUpload({ createParentPath: true }), createPostController.createPost)
 
+
 router.route('/posts/:postUserID/:postId')
     .get(checkJWT.check, verifyAccessToken.verifyAccess, getSinglePostController.getSinglePost)
     .put(checkJWT.check, verifyAccessToken.verifyAccess, editPostController.editPost)
+    .delete(checkJWT.check, verifyAccessToken.verifyAccess, deletePostController.deletePost)
+
+
 
 module.exports = router;
