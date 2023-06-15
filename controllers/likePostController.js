c821a3b5be304e01f5f572bafcbc72bb88278456
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-15 21:04:14 +0600

    Add files via upload

---
+++ controllers/likePostController.js
@@ -0,0 +1,48 @@
+const Likes = require('../models/Likes');
+const Post = require('../models/Post');
+
+const likePost = async (req, res) => {
+    const author = req.params.userID;
+    const postId = req.params.postId;
+
+    const loggedInUserID = req.userID;
+
+    const foundPost = await Post.findOne({ $and: [{ userID: author }, { postId: postId }] });
+
+    if (!foundPost) {
+        return res.status(404).json({
+            error: true,
+            message: "post not found"
+        })
+    }
+
+    const foundLike = await Likes.findOne({ $and: [{ userID: loggedInUserID }, { postId: postId }] });
+
+    if (foundLike) {
+        return res.status(200).json({
+            message: "post was already liked"
+        })
+    }
+
+    const createdLike = await Likes.create({
+        userID: loggedInUserID,
+        postId: postId
+    });
+
+    await createdLike.save();
+
+    foundPost.likes = await foundPost.likes + 1;
+    await foundPost.save();
+
+    return res.status(200).json({
+        success: true,
+        message: "Post Liked"
+    })
+
+
+
+}
+
+module.exports = {
+    likePost
+};
