688be1867269e2d2948dafcbe68d55465959be73
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-19 13:26:01 +0600

    Add files via upload

--- routes/likes.js
+++ routes/likes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const likePostController = require('../controllers/likePostController');
+const unlikePostController = require('../controllers/unlikePostController');
 const verifyAccess = require('../middlewares/verifyAccessToken').verifyAccess;
 const checkJWT = require('../middlewares/checkJWT');
 
 router.route('/like/:userID/:postId')
     .get(checkJWT.check, verifyAccess, likePostController.likePost)
+
+router.route('/unlike/:userID/:postId')
+    .get(checkJWT.check, verifyAccess, unlikePostController.unlikePost);
 
 
 module.exports = router;
