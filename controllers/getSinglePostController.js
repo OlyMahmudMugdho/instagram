36554dca9f85d532b5859c5b1733eae275a99d85
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-19 14:24:14 +0600

    Add files via upload

--- controllers/getSinglePostController.js
+++ controllers/getSinglePostController.js
@@ -1,9 +1,25 @@
+const Followers = require('../models/Followers');
 const Post = require('../models/Post');
 
 const getSinglePost = async (req, res) => {
     const userID = req.params.postUserID;
     const postId = req.params.postId;
 
+    if (!userID || !postId) {
+        return res.status(404).json({
+            error: true,
+            message: "userID and postID are required"
+        });
+    }
+
+    const existedFollower = await Followers.findOne({ $and: [{ follower: req.userID }, { following: userID }] });
+
+    if (!existedFollower) {
+        return res.status(403).json({
+            error: true,
+            message: "you are not following the user"
+        });
+    }
 
     const foundPost = await Post.findOne({ userID: userID, postId: postId });
 
@@ -15,6 +31,7 @@ const getSinglePost = async (req, res) => {
     }
 
     return res.status(200).json({
+        success: true,
         data: foundPost
     });
 }
