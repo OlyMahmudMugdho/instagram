1b1eb4c51c824ca71c67d08a86ebe4cb58995c8a
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-20 11:23:41 +0600

    Add files via upload

--- routes/follow.js
+++ routes/follow.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
 const followersController = require('../controllers/followersController');
+const unfollowController = require('../controllers/unfollowController');
 const checkJWT = require('../middlewares/checkJWT');
 const verifyAccessToken = require('../middlewares/verifyAccessToken');
 
 router.route('/follow/:followingID')
     .get(checkJWT.check, verifyAccessToken.verifyAccess, followersController.follow)
+
+router.route('/unfollow/:followingID')
+    .get(checkJWT.check, verifyAccessToken.verifyAccess, unfollowController.unfollow)
 
 
 
