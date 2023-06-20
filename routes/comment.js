077d7f4c1e176f6c4108738882d7f170b9242b5d
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-20 18:09:40 +0600

    Add files via upload

--- routes/comment.js
+++ routes/comment.js
@@ -2,9 +2,14 @@ const router = require('express').Router();
 const addCommentController = require('../controllers/addCommentController');
 const checkJWT = require('../middlewares/checkJWT');
 const verifyAccessToken = require('../middlewares/verifyAccessToken');
+const editCommentController = require('../controllers/editCommentController').editComment;
+
 
 router.route('/comment/:postUserID/:postId')
-    .post(checkJWT.check, verifyAccessToken.verifyAccess, addCommentController.addComment);
+    .post(checkJWT.check, verifyAccessToken.verifyAccess, addCommentController.addComment)
+
+    router.route('/comment/:postUserID/:postId/:commentID')
+    .put(checkJWT.check, verifyAccessToken.verifyAccess, editCommentController)
 
 
 module.exports = router;
