b2cdf187c5508693d991ca9fc7fc999962f89359
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-07-26 18:02:47 +0600

    Add files via upload

--- controllers/newsFeedController.js
+++ controllers/newsFeedController.js
@@ -1,1 +1,24 @@
 const Posts = require('../models/Post');
+const Followers = require('../models/Followers');
+const Comments = require('../models/Followers');
+
+
+const getFeed = async (req, res) => {
+    const userID = req.userID;
+    const foundFollowing = await Followers.find({follower : userID});
+    const followingIDs = await foundFollowing.map(
+        item => item.following
+    )
+
+    console.log("from newsfeed controller");
+    console.log(followingIDs);
+    
+    // const matchedFollowers =
+    const foundPosts = await Posts.find({ userID : { $in : followingIDs } }).sort({ date : -1 });
+    
+    return res.status(200).json(foundPosts);
+}
+
+module.exports = {
+    getFeed
+};
