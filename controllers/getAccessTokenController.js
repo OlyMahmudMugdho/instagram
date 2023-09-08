99ba9767dec2065716ff21929e9e51713ec63700
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-09-08 23:25:33 +0600

    Add files via upload

--- controllers/getAccessTokenController.js
+++ controllers/getAccessTokenController.js
@@ -1,20 +1,31 @@
-
+const Users = require('../models/Users');
+const User = require('../models/Users');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const getAccessToken = (req, res) => {
+const getAccessToken = async (req, res) => {
     const cookies = req.cookies;
 
     if (!cookies || !cookies.jwt) {
         return res.status(403).json(
-            {
-                "message": "unauthenticated"
+            {   "error" : "no refreshToken",
+                "message": "unauthenticated",
             }
         );
     }
 
+    jwt.verify(cookies.jwt, process.env.REFRESH_TOKEN_SECRET, (error,decoded) => {
+        if(error) {
+            return res.status(403).json({ "message": error.message })
+        }
+        req.username = decoded.username,
+        req.userID = decoded.userID
+    } )
+        
+
+
     const accessToken = jwt.sign(
-        { "username": req.username, "userID" : req.userID  },
+        { "username": req.username, "userID": req.userID },
         process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn: '30000s'
