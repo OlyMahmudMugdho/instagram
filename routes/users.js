9ef2875ae08ca84a16e05636b39fef594f6da621
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-09-12 13:30:07 +0600

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const checkJWT = require('../middlewares/checkJWT');
 const verifyAccessToken = require('../middlewares/verifyAccessToken');
 const getPaginatedUsersController = require("../controllers/getPaginatedUsersController");
+const getRandomUsersController = require("../controllers/getRandomUsersController");
+
+router.route('/users')
+    .get(checkJWT.check, verifyAccessToken.verifyAccess, getRandomUsersController.getRandomUsers)
 
 router.route('/users/:page')
     .get(checkJWT.check, verifyAccessToken.verifyAccess, getPaginatedUsersController.getUsers);
