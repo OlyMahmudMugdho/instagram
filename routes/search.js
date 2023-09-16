c98d97b6c224153cb6005cf8eeb70a8f852262fb
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-09-16 21:05:29 +0600

    added search controller to routes

---
+++ routes/search.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const searchController = require('../controllers/searchController');
+const checkJWT = require('../middlewares/checkJWT');
+const verifyAccessToken = require('../middlewares/verifyAccessToken');
+const verifyJWT = require('../middlewares/verifyJWT');
+
+router.route('/search')
+    .get(checkJWT.check, verifyAccessToken.verifyAccess, searchController.search)
+
+module.exports = router
