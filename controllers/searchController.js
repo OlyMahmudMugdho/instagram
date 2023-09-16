e1f337a409adca36402c643a8057753f4c070202
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-09-16 21:04:19 +0600

    implemented search feature

---
+++ controllers/searchController.js
@@ -0,0 +1,40 @@
+
+const Users = require('../models/Users');
+
+const search = async (req, res) => {
+    const searchQuery = req.query.q;
+    console.log(searchQuery)
+    if (!search) {
+        return res.status(404).json({
+            error: true,
+            message: "found nothing"
+        })
+    }
+
+    const foundMatched = await Users.find({
+        $or : [
+            {name: { $regex: searchQuery, $options: 'i' }},
+            {username : {$regex : searchQuery}}
+        ]
+
+    });
+
+    let foundNames = [];
+    let foundUsernames = [];
+
+    await foundMatched.map( result => foundNames.push(result.name) && foundUsernames.push(result.username))
+
+    console.log(await foundMatched);
+
+    return res.status(200).json({
+        success: true,
+        result: {
+            names : foundNames,
+            usernames : foundUsernames
+        }
+    })
+}
+
+module.exports = {
+    search
+}
