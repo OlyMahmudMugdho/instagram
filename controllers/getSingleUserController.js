a4142ffe229d40a588678a2d65b21b566ade356f
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-09-28 10:40:41 +0600

    Add files via upload

---
+++ controllers/getSingleUserController.js
@@ -0,0 +1,30 @@
+const User = require('../models/Users');
+
+const getSingleUser = async (req, res) => {
+    const userID = req.params.userID;
+
+    if (!userID) {
+        return res.status(404).json({
+            error: true,
+            message: "invalid or blank user ID"
+        })
+    }
+
+    const foundUser = await User.findOne({ userID: userID });
+
+    if (!foundUser) {
+        return res.status(404).json({
+            error: true,
+            message: "User not found"
+        })
+    }
+
+    return res.status(200).json({
+        success : true,
+        message  : foundUser
+    })
+}
+
+module.exports = {
+    getSingleUser
+}
