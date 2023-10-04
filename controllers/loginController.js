33b8de13109f325e3d76b4dc60f3ab08a86249a9
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-10-04 14:10:22 +0600

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -53,7 +53,7 @@ const handleLogin = async (req, res) => {
     return res.status(200).header('Access-Control-Allow-Credentials',true).cookie(
         'jwt',
         refreshToken,
-        {     httpOnly: true, secure: true, expiresIn: '60*60*1000s' }
+        {     httpOnly: true, secure: true, sameSite:'None', expiresIn: '60*60*1000s' }
     ).json(
         {
             "refreshToken": refreshToken,
