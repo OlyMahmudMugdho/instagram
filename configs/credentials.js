ec5762cf32be0dc0f9aad6122038aba1c4903304
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-10-07 01:34:01 +0600

    Add files via upload

--- configs/credentials.js
+++ configs/credentials.js
@@ -1,6 +1,9 @@
 
 const credentials = (req,res,next) => {
     res.header('Access-Control-Alllow-Credentials',true);
+    res.header('Access-Control-Allow-Origin',true) ,
+    res.header('Access-Control-Allow-Methods',true),
+    res.header('Access-Control-Allow-Headers',true)
     next();
 }
 
