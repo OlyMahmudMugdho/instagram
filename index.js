b1e2a0f58806e457651f4155b5700c2cb3a9aece
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-10-08 12:04:18 +0600

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dbConnection.connectDB();
 
 app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Origin','http://localhost:5173');
   next();
 });
 
