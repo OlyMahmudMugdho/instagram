15c5e6a0894a8cabcb24d6db4382b6007514ca30
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-10-08 11:23:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 dbConnection.connectDB();
 
 app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Headers', '*');
 
   next();
