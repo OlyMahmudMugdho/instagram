40784c3c8ca60bddab305bd37bb65ac96f0ed0ea
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-10-08 11:56:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const corsConfig = {
 
 // app.options('*', cors())
 
-app.use(cors());
+app.use(cors({origin : '*'}));
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
