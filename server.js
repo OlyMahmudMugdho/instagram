51cc87def719e6a992b5d9229ee2ce9c270f56f2
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-10-09 18:56:16 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ dbConnection.connectDB();
 app.use(express.static(path.join(__dirname,'client/build/')))
 
 app.use(bodyParser.urlencoded({ extended: false }));
-const corsOptions = ['https://www.onrender.com'];
+const corsOptions = ['https://*.onrender.com'];
 const corsConfig = {
     credentials: true,
     origin: (origin, callback) => {
