cf146a3909cc5cc4e12d90794578b966372824f9
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-10-09 18:29:35 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 dbConnection.connectDB();
 
-app.use(express.static(path.join(__dirname,'pixl/dist/')))
+app.use(express.static(path.join(__dirname,'client/build/')))
 
 app.use(bodyParser.urlencoded({ extended: false }));
 const corsOptions = ['*', 'https://example.com'];
@@ -42,7 +42,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.
-        sendFile(path.join(__dirname, 'pixl', 'dist', 'index.html'));
+        sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 
