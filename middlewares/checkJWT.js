7ea150e644c04c65f059bf04d554c92f32fc6eb3
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-10-25 01:01:09 +0600

    Add files via upload

--- middlewares/checkJWT.js
+++ middlewares/checkJWT.js
@@ -2,7 +2,7 @@ const check = async (req, res, next) => {
     // const cookie = req.cookies;
     const cookie = req.headers['authorization'];
     
-    if (!cookie || !cookie.jwt) {
+    if (!cookie && !cookie.jwt) {
         return res.status(403).json({ "message": "unauthenticated" });
     }
     
