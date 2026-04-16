3a2939a060aff541b0f163b57b9d1ca587c2c4d3
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2026-04-16 20:24:39 +0600

    update layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout() {
   return (
     <PaperProvider theme={theme}>
       <AuthProvider>
-        <Stack />
+        <Stack screenOptions={{ headerShown: false }} />
       </AuthProvider>
     </PaperProvider>
   );
